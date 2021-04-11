ffd084ea2f0e86c29e3b941d24a6bbd8d99a1e89
Julia-Ponomarenko <yuliasha_02@mail.ru>
2021-04-11 23:46:37 +0300

    practice_3.7

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,56 +1,49 @@
-import java.util.Arrays;
+
+import org.junit.runners.model.TestClass;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class MainClass {
-    public static void main(String[] args)
-    {
-//        int [] x = {1,3,5,4,8,8};
-//        int [] y = {1,4,2,4,1,4};
-//        getPartAfter4(x);
-//        check1And4(y);
-//        System.out.println(check1And4(y));
+    public static void main(String[] args) {
+        try {
+            start(Test1.class);
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
-    public static int [] getPartAfter4  (int  [] arr) throws RuntimeException {
-            int[] newArr = null;
-            int i = arr.length - 1;
-            int j = 0;
-            boolean found = false;
-            while (i >= 0 && !found) {
-                found = arr[i] == 4;
-                j = i;
-                i--;
-            }
-            if(found) {
-                newArr = new int[arr.length - j - 1];
-                for (i = j + 1; i < arr.length; i++) {
-                    newArr[i - j - 1] = arr[i];
-                }
-            } else {
-                throw new RuntimeException("В массиве нет числа 4");
-            }
-//            System.out.println(Arrays.toString( newArr));
-            return  newArr;
-        }
-        public static boolean check1And4(int[] arr) {
-        boolean check = true;
-        int count1 = 0;
-        int count4 = 0;
-        int i = 0;
-        while (i < arr.length && check) {
-            if(arr[i] == 1) {
-                check = true;
-                count1++;
-            } else {
-                if(arr[i] == 4) {
-                    check = true;
-                    count4++;
-                } else {
-                    check = false;
-                }
-            }
-            i++;
-        }
-        return check && count1 > 0 && count4 > 0;
 
+    public static void start(Class c) throws InvocationTargetException, IllegalAccessException {
+        Method[] methods = c.getDeclaredMethods();
+        List<Method> list = new ArrayList<> ();
+        for (Method m : methods) {
+            if (m.isAnnotationPresent (Test.class))
+            {
+                list.add(m);
+            }
+        }
+        list.sort(new Comparator<Method>() {
+            @Override
+            public int compare(Method m1, Method m2) {
+                return m2.getAnnotation(Test.class).priority () - m1.getAnnotation (Test.class).priority ();
+            }
+        });
+        for (Method m: methods) {
+            if (m.isAnnotationPresent(BeforeSuite.class)) {
+                list.add(0, m);
+            }
+            if (m.isAnnotationPresent(AfterSuite.class)){
+                    list.add(m);
+            }
+        }
+        for (Method m: list){
+            m.invoke (null);
+        }
     }
 }
 
--- src/Test1.java
+++ src/Test1.java
@@ -1,38 +1,13 @@
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.Test;
-import java.util.Arrays;
-import java.util.Collection;
-
-@RunWith(Parameterized.class)
 public class Test1 {
-    @Parameterized.Parameters
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][]{
-                {new int[]{8, 8}, new int[]{1, 3, 5, 4, 8, 8}},
-                {new int[]{8, 2}, new int[]{1, 3, 5, 4, 8, 8}},
-                {new int[]{2, 3, 7, 0}, new int[]{6, 4, 2, 3, 7, 1}},
-                {new int[]{2, 3, 7, 1}, new int[]{6, 4, 2, 3, 7, 1}},
-                {new int[]{2, 3}, new int[]{6, 2, 2, 3, 7, 1}},
-        });
-    }
-    private MainClass mainClass;
-    private int[] x;
-    private int[] y;
-    public Test1(int[] x, int[] y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    @Before
-    public void init() {
-        mainClass = new MainClass();
-    }
-
+    public static void notTest (){System.out.println("NotTest");}
+    @BeforeSuite
+    public static void beforeSuite (){System.out.println("BeforeSuite");}
+    @Test (priority = 3)
+    public static void test1 (){System.out.println("Test1");}
     @Test
-    public void test1() {
-        Assert.assertTrue("Массивы не совпадают", Arrays.equals(x, mainClass.getPartAfter4(y)));
-    }
+    public static void test2 (){System.out.println("Test2");}
+    @Test (priority = 9)
+    public static void test3 (){System.out.println("Test3");}
+    @AfterSuite
+    public static void afterSuite (){System.out.println("AfterSuite");}
 }
